9dbf0bbf96aa5cfd2a74d116b3cc2b83112da7cd
Edima Udo <edimaudo@gmail.com>
2018-05-27 18:55:17 -0400

    compression algorithm

---
+++ scompression.py
@@ -0,0 +1,20 @@
+#aabbccdd - abcd
+
+#aaabbbbccddd -> a2b4cd3
+
+import random
+import sys
+import math
+
+def main():
+	try:
+		#do something
+		count = 0
+		
+	except:
+		e = sys.exc_info()
+		print(e)
+		sys.exit(1)
+
+if "__name__" == "__main__":
+	main()
