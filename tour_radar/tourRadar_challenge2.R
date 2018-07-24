cb92a846d0132c5dcd78243f99d21d069532f768
Edima Udo <edimaudo@gmail.com>
2018-07-24 18:51:05 -0400

    update for tourRadar Challenge 2

--- tour_radar/tourRadar_challenge2.R
+++ tour_radar/tourRadar_challenge2.R
@@ -20,56 +20,47 @@ basic_stats <- summary(df)
 print(basic_stats)
 
 #missing data
+missing_data <- apply(df, 2, function(x) any(is.na(x)))
 
-#outliers
 
+
+#-------------------
 #exploratory analysis
+#-------------------
 
-# each variable
-# 
-# most popular path
-# least popular path
-# 
-# highest number of sessions
-# lowest number of sessions
-# average # of sessions
-# 
-# highest, lowest, average bounce rate
-# 
-# highest, lowest, average time of page
-# 
-# highest, lowest, average transactions
-# 
-# 
-# graphs
+#sessions          bounces          time_on_page     transactions
+
 # sessions vs bounce rate
+ggplot(data=df, aes(y=sessions, x=bounces)) +
+  geom_line()+ geom_point() + theme_classic()
 # sessions vs time on page
+ggplot(data=df, aes(y=sessions, x=time_on_page)) +
+  geom_line()+ geom_point() + theme_classic()
 # sessions vs transactions
-# 
+ggplot(data=df, aes(y=sessions, x=transactions)) +
+  geom_line()+ geom_point() + theme_classic()
+
 # bounce rate vs time on page
 # bounce rate vs transactions
 # 
 # time on page vs transactions
 # 
+
 # correlations
-# sessions, bounce rate
-# sessions, time on page
-# sessions, transactions
-# 
-# bounce rate, time on page
-# bounce rate, transactions
-# 
-# time on page, transactions
-# 
-# 
+corrinfo <- df[,3:6]
+corrplot(cor(corrinfo), method="number")
+
+
+
 # trends over time
 # date vs path
 # date vs sessions
 # date vs transactions
 # date vs bounce rate
 # date vs time on page
+ggplot(data=df, aes(y=sessions, x=transactions)) +
+  geom_line(color="blue")+ geom_point() + theme_classic()
 
-#analytics
-#rfm model
+#rfm modelling
 
-#basic forecasting using prophet with other infor
+#time series
