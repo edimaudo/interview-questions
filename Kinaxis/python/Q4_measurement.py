ec76bc5a851f6ca62e8e2ce852e235295b8f2cea
Edima <edimaudo@gmail.com>
2021-06-19 00:39:40 -0400

    Update Q4_measurement.py

--- Kinaxis/python/Q4_measurement.py
+++ Kinaxis/python/Q4_measurement.py
@@ -13,13 +13,17 @@ df_item_week ['WMAPE'] = df_item_week ['units'] - df_item_week ['forecast']
 df_item_week ['WMAPE'] = df_item_week ['WMAPE'].abs()
 df_item_week ['WMAPE'] = df_item_week ['WMAPE'] / df_item_week ['units']*df_item_week ['units']
 WMAPE = df_item_week['WMAPE'].sum()/df_item_week ['units'].sum()                   
-ITEM_WEEK = [WMAPE]
+ITEM_WEEK = [WMAPE, 0.8*WMAPE, 0.2*WMAPE]
 
 # WMAPE FOR STORE CATEGORY WEEK LEVEL
 df_store_category_week = df.groupby(['store','category','week']).aggregate({'units': 'sum',
                              'forecast': 'sum'}).reset_index()
 df_store_category_week ['WMAPE'] = df_store_category_week ['units'] - df_store_category_week ['forecast']
 df_store_category_week ['WMAPE'] = df_store_category_week ['WMAPE'].abs()
-df_store_category_week ['WMAPE'] = df_store_category_week ['WMAPE'] / df_store_category_week ['units']*f_store_category_week ['units']
+df_store_category_week ['WMAPE'] = df_store_category_week ['WMAPE'] / df_store_category_week ['units']*df_store_category_week ['units']
 WMAPE = df_store_category_week['WMAPE'].sum()/df_store_category_week ['units'].sum()
-STORE_CATEGORY_WEEK = [WMAPEß]
+STORE_CATEGORY_WEEK = [WMAPE, 0.8*WMAPE, 0.2*WMAPE]
+
+df_week_info = [ITEM_WEEK,STORE_CATEGORY_WEEK]
+df_final = pd.DataFrame(df_week_info, index = ['Item week','Store Category Week'], columns =['WMAPE','OC','UC'])
+print(df_final)
