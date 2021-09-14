e2fec1b342422e4b283e91a40a01b28264149f4d
Edima <edimaudo@gmail.com>
2021-09-14 18:03:10 -0400

    Update segmentation_analysis.R

--- crescendo/segmentation_analysis.R
+++ crescendo/segmentation_analysis.R
@@ -20,4 +20,12 @@ for (package in packages) {
 
 
 # Load Data
-segmentation_df <- read_excel(file.choose())
+player_wager <- read_excel("Player segmentation dummy data trimmed.xlsx",sheet="Player Wagering data")
+player_signup <- read_excel("Player segmentation dummy data trimmed.xlsx",sheet="Player sign up stats") 
+player_transaction <- read_excel("Player segmentation dummy data trimmed.xlsx",sheet="Player Transaction Stats")
+
+# Summary
+summary(player_wager)
+summary(player_signup)
+summary(player_transaction)
+
