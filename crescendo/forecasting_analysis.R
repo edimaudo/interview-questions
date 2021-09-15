82e780de6f8a871fd801fad8549884735a6d0b4e
Edima <edimaudo@gmail.com>
2021-09-15 11:56:31 -0400

    Update forecasting_analysis.R

--- crescendo/forecasting_analysis.R
+++ crescendo/forecasting_analysis.R
@@ -20,17 +20,54 @@ for (package in packages) {
 
 forecast_df <- read_excel("Forecast Test Data - Senior BI Analyst.xlsx",sheet="Data")
 
+forecast_overall_df <- forecast_df %>%
+  group_by(Year, WeekNo) %>%
+  dplyr::summarise(Turnover_total = sum(Turnover), 
+            Profit_total = sum(Profit), 
+            CustomerCount_total = sum(CustomerCount)) %>%
+  select(Year, WeekNo, Turnover_total, Profit_total, CustomerCount_total)
+
+forecast_turnover <- ggplot(forecast_overall_df, aes(x=as.factor(WeekNo), 
+                                                           y=Turnover_total, 
+                                                           group = as.factor(Year))) +
+  geom_line(aes(color=Year)) +
+  geom_point(aes(color=Year)) +
+  theme_minimal() + scale_y_continuous(labels = comma) +
+  ggtitle("Turnover trend")+
+  xlab("Week #") + 
+  ylab("Turnover") + scale_colour_discrete(name="Year")
+
+forecast_turnover
+
+forecast_Profit <- ggplot(forecast_overall_df, aes(x=as.factor(WeekNo), 
+                                                   y=Profit_total, 
+                                                   group = as.factor(Year))) +
+  geom_line(aes(color=Year)) +
+  geom_point(aes(color=Year)) +
+  theme_minimal() + scale_y_continuous(labels = comma) +
+  ggtitle("Profit trend")+
+  xlab("Week #") + 
+  ylab("Profit") + scale_colour_discrete(name="Year")
+
+forecast_Profit
+
+forecast_CustomerCount <- ggplot(forecast_overall_df, aes(x=as.factor(WeekNo), 
+                                                          y=CustomerCount_total, 
+                                                          group = as.factor(Year))) +
+  geom_line(aes(color=Year)) +
+  geom_point(aes(color=Year)) +
+  theme_minimal() + scale_y_continuous(labels = comma) +
+  ggtitle("CustomerCount trend")+
+  xlab("Week #") + 
+  ylab("CustomerCount") + scale_colour_discrete(name="Year")
+
+forecast_CustomerCount
+
+
+
 # Visualize the time series (aggregate the info) 
 
 # Overall
-forecast_overall_df <- forecast_df %>%
-  group_by(Year, WeekNo) %>%
-  summarise(Turnover_total = sum(Turnover), 
-            Profit_total = sum(Profit), 
-            CustomerCount_total = sum(CustomerCount)) %>%
-  select(Year, WeekNo, Turnover, Profit_Total, CustomerCount_total)
-  
-
 
 # By website
 
