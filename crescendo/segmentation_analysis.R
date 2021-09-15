0e0cb81b5299cd5e4383a3ff89046af4a23f96ba
Edima <edimaudo@gmail.com>
2021-09-14 21:10:58 -0400

    Update segmentation_analysis.R

--- crescendo/segmentation_analysis.R
+++ crescendo/segmentation_analysis.R
@@ -1,4 +1,6 @@
+#===================
 # Segmentation analysis
+#===================
 
 rm(list = ls()) #clear environment
 
@@ -9,8 +11,6 @@ packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies","fastDummies",
               'plyr','mlbench','caTools','doParallel',
               'scales','dplyr','mlbench','caTools','lubridate',
               'grid','gridExtra','doParallel','readxl')
-
-# Load packages
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
     install.packages(package)
@@ -18,8 +18,9 @@ for (package in packages) {
   }
 }
 
-
+#===================
 # Load Data
+#===================
 player_wager <- read_excel("Player segmentation dummy data trimmed.xlsx",sheet="Player Wagering data")
 player_signup <- read_excel("Player segmentation dummy data trimmed.xlsx",sheet="Player sign up stats") 
 player_transaction <- read_excel("Player segmentation dummy data trimmed.xlsx",sheet="Player Transaction Stats")
@@ -36,57 +37,59 @@ summary(player_transaction)
 # Country cumulative Age
 Country_age <- player_signup %>%
   filter(!is.na(YearOfBirth)) %>%
-  mutate(Age = lubridate::year(now()) - YearOfBirth)
-
-Country_age <- Country_age %>%
+  mutate(Age = lubridate::year(now()) - YearOfBirth) %>%
   group_by(Country) %>%
   dplyr::summarise(age_total = sum(Age)) %>%
   select(Country, age_total) %>%
   arrange(desc(age_total)) %>%
   top_n(20) %>%
-  ggplot(aes(x = Country, y = age_total)) + 
+  ggplot(aes(x = reorder(Country,age_total), y = age_total)) + 
   geom_bar(stat = "identity") + 
-  coord_flip() + 
+  coord_flip() + theme_minimal() +
   guides(fill = FALSE) + 
-  ggtitle("Country vs. Age") + 
+  ggtitle("Top 20 Countries vs. Cumulative Age") + 
   xlab("Country") + 
-  ylab("Age")
+  ylab("Cumulative Age")
+Country_age
 
+# Country user count
+Country_user_count <- player_signup %>%
+  filter(!is.na(YearOfBirth)) %>%
+  group_by(Country) %>%
+  dplyr::summarise(user_total = n()) %>%
+  select(Country, user_total) %>%
+  arrange(desc(user_total)) %>%
+  top_n(20) %>%
+  ggplot(aes(x = reorder(Country,user_total), y = user_total)) + 
+  geom_bar(stat = "identity") + 
+  coord_flip() + theme_minimal() +
+  guides(fill = FALSE) + 
+  ggtitle("Top 20 Countries vs. User Count") + 
+  xlab("Country") + 
+  ylab("User Count")
+Country_user_count
 
+## Country 1 has the most users and some of the oldest users  
 
+# Country turnover
 
-
-
-user_count = n()
+# Country Profit
   
+# Country WagerCount
 
+# Country DaysPlayed)
 
-
-
-
-
-
-
-
-
-user_count = dplyr::tally(CustomerID)
-
-(Country	Cumulative age)
-(Country	turnover	
-  Profit	
-  WagerCount	
-  DaysPlayed)
-Country vs transaction type
-
-
-#===================
-# Country segmentation
-#===================
-
+# Country transaction type
 
 
 #===================
 # Sport segmentation
+#===================
+
+
+
+#===================
+# Customer segmentation
 #===================
 
 
