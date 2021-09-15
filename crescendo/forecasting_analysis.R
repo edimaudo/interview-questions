89b64a614dec9249096a7b2a8de7fc656ca5255e
Edima <edimaudo@gmail.com>
2021-09-14 22:17:11 -0400

    Update forecasting_analysis.R

--- crescendo/forecasting_analysis.R
+++ crescendo/forecasting_analysis.R
@@ -6,7 +6,7 @@ rm(list = ls()) #clear environment
 # Packages
 #=============
 packages <- c('ggplot2', 'corrplot','tidyverse','readxl',
-              'shiny','shinydashboard','scales','dplyr','mlbench','caTools',
+              'scales','dplyr','mlbench','caTools','grid','gridExtra',
               'forecast','TTR','xts','lubridate')
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
@@ -20,3 +20,20 @@ for (package in packages) {
 
 forecast_df <- read_excel("Forecast Test Data - Senior BI Analyst.xlsx",sheet="Data")
 
+# Convert Year-XX to Numerical data
+#  Generate Year-month and pick first day to convert to date
+# Generate view 
+
+# Visualize the time series (aggregate the info) 
+#- overall
+#- sports
+#- website
+
+#Forecasting using different techniques
+#Overall forecast
+#- Forecast by year
+#- Forecast by month
+
+#Forecast by website
+#- Forecast by year
+#- Forecast by month
