73fda548cd4c5a1a5ac973681efe47e5a9ad2ed7
Edima <edimaudo@gmail.com>
2021-09-15 17:10:58 -0400

    Update segmentation_analysis.R

--- crescendo/segmentation_analysis.R
+++ crescendo/segmentation_analysis.R
@@ -9,7 +9,7 @@ rm(list = ls()) #clear environment
 #===================
 packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies","fastDummies",'dplyr',
               'plyr','mlbench','caTools','doParallel','stringr',
-              'scales','dplyr','mlbench','caTools','lubridate',
+              'scales','dplyr','mlbench','caTools','lubridate','factoextra',
               'grid','gridExtra','readxl','NbClust','psy','nFactors')
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
@@ -20,17 +20,166 @@ for (package in packages) {
 #===================
 # Load Data
 #===================
-player_wager <- read_excel("Player segmentation dummy data trimmed.xlsx",sheet="Player Wagering data")
-player_signup <- read_excel("Player segmentation dummy data trimmed.xlsx",sheet="Player sign up stats") 
-player_transaction <- read_excel("Player segmentation dummy data trimmed.xlsx",sheet="Player Transaction Stats")
-
+player_wager <- read_excel("Player segmentation dummy data trimmed.xlsx",
+                           sheet="Player Wagering data")
+player_signup <- read_excel("Player segmentation dummy data trimmed.xlsx",
+                            sheet="Player sign up stats") 
+player_transaction <- read_excel("Player segmentation dummy data trimmed.xlsx",
+                                 sheet="Player Transaction Stats")
+#===================
 # Summary
+#===================
 summary(player_wager)
 summary(player_signup)
 summary(player_transaction)
 
 #===================
-# Country
+# Sports Visualization
+#===================
+# Sport user count
+Sport_user_count <- player_wager %>%
+  group_by(Sport) %>%
+  dplyr::summarise(user_total = n()) %>%
+  select(Sport, user_total) %>%
+  arrange(desc(user_total)) %>%
+  top_n(20) %>%
+  ggplot(aes(x = reorder(Sport,user_total), y = user_total)) + 
+  geom_bar(stat = "identity") + 
+  coord_flip() + theme_minimal() +
+  guides(fill = FALSE) + scale_y_continuous(labels = comma)
+ggtitle("Top 20 Countries vs. User Count") + 
+  xlab("Sport") + 
+  ylab("User Count")
+Sport_user_count
+
+# Sport turnover
+Sport_turnover <- player_wager %>%
+  group_by(Sport) %>%
+  dplyr::summarise(turnover_total = sum(Turnover)) %>%
+  arrange(desc(turnover_total), Sport) %>%
+  select(Sport, turnover_total) %>%
+  top_n(20) %>%
+  ggplot(aes(x = reorder(Sport,turnover_total), y = turnover_total)) + 
+  geom_bar(stat = "identity") + 
+  coord_flip() + theme_minimal() +
+  guides(scale = "none") + scale_y_continuous(labels = comma) +
+  ggtitle("Top 20 Sports vs. Total Turnover") + 
+  xlab("Sport") + 
+  ylab("Turnover")
+Sport_turnover
+
+# Sport Profit
+Sport_profit <- player_wager %>%
+  group_by(Sport) %>%
+  dplyr::summarise(profit_total = sum(Profit)) %>%
+  arrange(desc(profit_total), Sport) %>%
+  select(Sport, profit_total) %>%
+  top_n(20) %>%
+  ggplot(aes(x = reorder(Sport,profit_total), y = profit_total)) + 
+  geom_bar(stat = "identity") + 
+  coord_flip() + theme_minimal() +
+  guides(scale = "none") + scale_y_continuous(labels = comma) +
+  ggtitle("Top 20 Sports vs. Total Profit") + 
+  xlab("Sport") + 
+  ylab("Profit")
+Sport_profit
+
+
+# Sport WagerCount
+Sport_wager <- player_wager %>%
+  group_by(Sport) %>%
+  dplyr::summarise(wager_total = sum(WagerCount)) %>%
+  arrange(desc(wager_total), Sport) %>%
+  select(Sport, wager_total) %>%
+  top_n(20) %>%
+  ggplot(aes(x = reorder(Sport,wager_total), y = wager_total)) + 
+  geom_bar(stat = "identity") + 
+  coord_flip() + theme_minimal() +
+  guides(fill = FALSE) + scale_y_continuous(labels = comma) +
+  ggtitle("Top 20 Sports vs. Wager Total") + 
+  xlab("Sport") + 
+  ylab("Wager")
+Sport_wager
+
+# Sport DaysPlayed
+Sport_days_played <- player_wager %>%
+  group_by(Sport) %>%
+  dplyr::summarise(days_played_total = sum(DaysPlayed)) %>%
+  arrange(desc(days_played_total), Sport) %>%
+  select(Sport, days_played_total) %>%
+  top_n(20) %>%
+  ggplot(aes(x = reorder(Sport,days_played_total), y = days_played_total)) + 
+  geom_bar(stat = "identity") + 
+  coord_flip() + theme_minimal() +
+  guides(fill = FALSE) + scale_y_continuous(labels = comma) + 
+  ggtitle("Top 20 Sports vs. Days Played Total") + 
+  xlab("Sport") + 
+  ylab("Days Played")
+Sport_days_played
+
+#======================
+# sports correlations & clustering
+#======================
+sport_wager <- player_wager %>%
+  group_by(Sport) %>%
+  dplyr::summarise(Turnover_total = sum(Turnover), 
+                   Profit_total = sum(Profit),
+                   WagerCount_total = sum(WagerCount),
+                   DaysPlayed_total = sum(DaysPlayed)) %>%
+  select(Sport, Turnover_total, Profit_total, WagerCount_total, DaysPlayed_total)
+
+sport_wager$Sport <- as.integer(stringr::str_sub(sport_wager$Sport,-2,-1))
+
+# Correlation
+corMat = cor(sport_wager)
+corrplot(corMat, method = 'number', order = "hclust", tl.col='black', tl.cex=.75) 
+# High correlations across all variables except sport
+
+# Clustering
+set.seed(1)
+fviz_nbclust(sport_wager,kmeans,method = "silhouette")
+
+clusterK <- kmeans(sport_wager,2,iter.max = 100,nstart = 50,algorithm = "Lloyd")
+clusterK
+pclust <- prcomp(sport_wager[-1],scale. = FALSE)
+summary(pclust)
+
+# Use optimal no. of clusters in k-means #
+k1=2 # compare with multiple k values
+
+# Generate K-mean clustering
+kfit <- kmeans(sport_wager, k1, nstart=25, iter.max=1000, 
+               algorithm = c("Hartigan-Wong", "Lloyd", "Forgy","MacQueen"), 
+               trace=FALSE)
+
+#Visualize clusters
+fviz_cluster(kfit, data = sport_wager, ellipse.type = "convex")+theme_minimal()
+fviz_cluster(kfit, data = sport_wager, geom = "point",stand = FALSE, 
+             ellipse.type = "norm")
+
+# No discernable insights from sports clustering
+
+#===================
+# Customer clustering
+#===================
+customer_wager <- player_wager %>%
+  group_by(CustomerID) %>%
+  dplyr::summarise(Turnover_total = sum(Turnover), 
+                   Profit_total = sum(Profit),
+                   WagerCount_total = sum(WagerCount),
+                   DaysPlayed_total = sum(DaysPlayed)) %>%
+  select(CustomerID, Turnover_total, Profit_total, WagerCount_total, DaysPlayed_total)
+
+corMat = cor(customer_wager)
+corrplot(corMat, method = 'number', order = "hclust", bg='#676767', tl.col='black', tl.cex=.75) 
+## no strong correlations
+
+
+set.seed(1)
+
+
+#===================
+# Country Visualization
 #===================
 # Country cumulative Age
 Country_age <- player_signup %>%
@@ -138,113 +287,14 @@ Country_days_played <- player_signup %>%
   xlab("Country") + 
   ylab("Days Played")
 Country_days_played
+
+
   
-#===================
-# Sport
-#===================
-# Sport user count
-Sport_user_count <- player_wager %>%
-  group_by(Sport) %>%
-  dplyr::summarise(user_total = n()) %>%
-  select(Sport, user_total) %>%
-  arrange(desc(user_total)) %>%
-  top_n(20) %>%
-  ggplot(aes(x = reorder(Sport,user_total), y = user_total)) + 
-  geom_bar(stat = "identity") + 
-  coord_flip() + theme_minimal() +
-  guides(fill = FALSE) + scale_y_continuous(labels = comma)
-ggtitle("Top 20 Countries vs. User Count") + 
-  xlab("Sport") + 
-  ylab("User Count")
-Sport_user_count
-
-
-# Sport turnover
-Sport_turnover <- player_wager %>%
-  group_by(Sport) %>%
-  dplyr::summarise(turnover_total = sum(Turnover)) %>%
-  arrange(desc(turnover_total), Sport) %>%
-  select(Sport, turnover_total) %>%
-  top_n(20) %>%
-  ggplot(aes(x = reorder(Sport,turnover_total), y = turnover_total)) + 
-  geom_bar(stat = "identity") + 
-  coord_flip() + theme_minimal() +
-  guides(scale = "none") + scale_y_continuous(labels = comma) +
-  ggtitle("Top 20 Sports vs. Total Turnover") + 
-  xlab("Sport") + 
-  ylab("Turnover")
-Sport_turnover
-
-
-# Sport Profit
-Sport_profit <- player_wager %>%
-  group_by(Sport) %>%
-  dplyr::summarise(profit_total = sum(Profit)) %>%
-  arrange(desc(profit_total), Sport) %>%
-  select(Sport, profit_total) %>%
-  top_n(20) %>%
-  ggplot(aes(x = reorder(Sport,profit_total), y = profit_total)) + 
-  geom_bar(stat = "identity") + 
-  coord_flip() + theme_minimal() +
-  guides(scale = "none") + scale_y_continuous(labels = comma) +
-  ggtitle("Top 20 Sports vs. Total Profit") + 
-  xlab("Sport") + 
-  ylab("Profit")
-Sport_profit
-#
-
-# Sport WagerCount
-Sport_wager <- player_wager %>%
-  group_by(Sport) %>%
-  dplyr::summarise(wager_total = sum(WagerCount)) %>%
-  arrange(desc(wager_total), Sport) %>%
-  select(Sport, wager_total) %>%
-  top_n(20) %>%
-  ggplot(aes(x = reorder(Sport,wager_total), y = wager_total)) + 
-  geom_bar(stat = "identity") + 
-  coord_flip() + theme_minimal() +
-  guides(fill = FALSE) + scale_y_continuous(labels = comma) +
-  ggtitle("Top 20 Sports vs. Wager Total") + 
-  xlab("Sport") + 
-  ylab("Wager")
-Sport_wager
-
-# Sport DaysPlayed
-Sport_days_played <- player_wager %>%
-  group_by(Sport) %>%
-  dplyr::summarise(days_played_total = sum(DaysPlayed)) %>%
-  arrange(desc(days_played_total), Sport) %>%
-  select(Sport, days_played_total) %>%
-  top_n(20) %>%
-  ggplot(aes(x = reorder(Sport,days_played_total), y = days_played_total)) + 
-  geom_bar(stat = "identity") + 
-  coord_flip() + theme_minimal() +
-  guides(fill = FALSE) + scale_y_continuous(labels = comma) + 
-  ggtitle("Top 20 Sports vs. Days Played Total") + 
-  xlab("Sport") + 
-  ylab("Days Played")
-Sport_days_played
 
 
 
-#===================
-# Customer
-#===================
-player_wager1 <- player_wager
-# remove "sport" from name
-player_wager1$Sport <- stringr::str_sub(player_wager1$Sport,start = -2, -1)
-player_wager1$Sport <- as.integer(player_wager1$Sport)
 
-corMat = cor(player_wager1)
-print(correlationMatrix)
-corrplot(corMat, method = 'number', order = "hclust", bg='#676767', tl.col='black', tl.cex=.75) 
-#corrplot(correlationMatrix,method='number',bg='#676767')
-# # summarize the correlation matrix
-
-
-
-# Player wager using K means clustering
-
+#print(NbClust(player_wager1, min.nc=2, max.nc=15, method="kmeans"))
 
 
 
