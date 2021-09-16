5fae54851e0a9f740aff59dd1553d8f365a2a175
Edima <edimaudo@gmail.com>
2021-09-15 21:40:08 -0400

    Update forecasting_analysis.R

--- crescendo/forecasting_analysis.R
+++ crescendo/forecasting_analysis.R
@@ -19,6 +19,8 @@ for (package in packages) {
 #===================
 
 forecast_df <- read_excel("Forecast Test Data - Senior BI Analyst.xlsx",sheet="Data")
+year_info <- read_excel("Forecast Test Data - Senior BI Analyst.xlsx",sheet="YearInfo")
+week_info <- read_excel("Forecast Test Data - Senior BI Analyst.xlsx",sheet="WeekInfo")
 
 #===================
 # Aggregated forecast data visualization
@@ -67,20 +69,19 @@ forecast_CustomerCount <- ggplot(forecast_overall_df, aes(x=as.factor(WeekNo),
 forecast_CustomerCount
 
 #==============
-# Forecast modeling
+# Forecast modelling
 #==============
 forecast_aggregate <- forecast_df %>%
-  group_by(Year, WeekNo, Website) %>%
+  inner_join(week_info,by = "WeekNo") %>%
+  inner_join(year_info,by = "Year") %>%
+  group_by(YearNo, Month, Website) %>%
   dplyr::summarise(Turnover_total = sum(Turnover), 
                    Profit_total = sum(Profit), 
                    CustomerCount_total = sum(CustomerCount)) %>%
-  select(Year, WeekNo, Website, Turnover_total, Profit_total, CustomerCount_total)
+  select(YearNo, Month, Website, Turnover_total, Profit_total, CustomerCount_total)
 
-# week num to month
 
-#convert year + month + add first day
 
-# convert to time series
 
 # Visualize the time series (aggregate the info) 
 
@@ -88,6 +89,94 @@ forecast_aggregate <- forecast_df %>%
 # Overall
 #==============
 patient.xts <- xts(x = df$Patients, order.by = df$Arrival_date) 
+patient.end <- floor(1*length(patient.monthly)) 
+patient.data <- ppatient.monthly[1:patient.end,] 
+patient.start <- c(year (start(patient.data)), month(start(patient.data)))
+patient.end <- c(year(end(patient.data)), month(end(patient.data)))
+patient.data <- ts(as.numeric(patient.data), start = patient.start, 
+                   end = patient.end, frequency = as.numeric(input$frequencyInput)) 
+
+#Decompose the Time Series
+patient.data %>%
+  decompose() %>%
+  autoplot()
+
+
+#Decompose the Time Series - multi season output
+patient.data %>%
+  mstl() %>%
+  autoplot()
+
+# ACF
+ggAcf(patient.data)
+
+
+patient.data <- apply.weekly(patient.xts, mean) 
+patient.end <- floor(as.numeric(input$traintestInput)*length(patient.data)) 
+patient.train <- patient.data[1:patient.end,] 
+patient.test <- patient.data[(patient.end+1):length(patient.data),]
+patient.start <- c(year (start(patient.train)), month(start(patient.train)))
+patient.end <- c(year(end(patient.train)), month(end(patient.train)))
+patient.train <- ts(as.numeric(patient.train), start = patient.start, 
+                    end = patient.end, frequency = as.numeric(input$frequencyInput) )
+patient.start <- c(year (start(patient.test)), month(start(patient.test)))
+patient.end <- c(year(end(patient.test)), month(end(patient.test)))
+patient.test <- ts(as.numeric(patient.test), start = patient.start, 
+                   end = patient.end, frequency = as.numeric(input$frequencyInput))
+
+# set forecast horizon
+forecast.horizon <- 10 #as.numeric(input$horizonInput) #horizon_info <- c(1:50)
+
+# models
+auto_exp_model <- patient.train %>% ets %>% forecast(h=forecast.horizon)
+#patient_train_auto_exp_forecast <- ets(patient.train) %>% 
+#  forecast(h=forecast.horizon) 
+
+auto_arima_model <- patient.train %>% auto.arima() %>% forecast(h=forecast.horizon)
+simple_exp_model <- patient.train %>% HoltWinters(beta=FALSE, gamma=FALSE) %>% 
+  forecast(h=forecast.horizon)
+double_exp_model <- patient.train %>% HoltWinters(beta = TRUE, gamma=FALSE) %>% 
+  forecast(h=forecast.horizon)
+triple_exp_model <- patient.train %>% HoltWinters(beta = TRUE, gamma = TRUE) %>% 
+  forecast(h=forecast.horizon)
+tbat_model <- patient.train %>% tbats %>% forecast(h=forecast.horizon)
+
+# forecast output
+auto_exp_forecast <- as.data.frame(patient_train_auto_exp_forecast$mean)
+auto_arima_forecast <- as.data.frame(patient_train_auto_arima_forecast$mean)
+simple_exp_forecast <- as.data.frame(patient_train_simple_exp_forecast$mean)
+double_exp_forecast <- as.data.frame(patient_train_double_exp_forecast$mean)
+triple_exp_forecast <- as.data.frame(patient_train_triple_exp_forecast$mean)
+tbat_forecast <- as.data.frame(patient_train_tbat_forecast$mean)
+manual_arima_forecast <- as.data.frame(patient_train_manual_forecast$mean)
+
+#model accuracy
+auto_exp_accuracy <- as.data.frame(accuracy(patient_train_auto_exp_forecast ,patient.test))
+auto_arima_accuracy <- as.data.frame(accuracy(patient_train_auto_arima_forecast ,patient.test))
+simple_exp_accuracy <- as.data.frame(accuracy(patient_train_simple_exp_forecast ,patient.test))
+double_exp_accuracy <- as.data.frame(accuracy(patient_train_double_exp_forecast ,patient.test))
+triple_exp_accuracy <- as.data.frame(accuracy(patient_train_triple_exp_forecast ,patient.test))
+tbat_accuracy <- as.data.frame(accuracy(patient_train_tbat_forecast ,patient.test))
+
+models<- c("auto-exponential","auto-exponential",
+           "auto-arima","auto-arima",
+           "simple-exponential","simple-exponential",
+           "double-exponential","double-exponential",
+           "triple-exponential","triple-exponential",
+           "tbat","tbat",
+           "manual-arima","manual-arima")
+
+data<- c("Training set", 'Test set',
+         "Training set", 'Test set',
+         "Training set", 'Test set',
+         "Training set", 'Test set',
+         "Training set", 'Test set',
+         "Training set", 'Test set',
+         "Training set", 'Test set')
+
+outputInfo <- rbind(auto_exp_accuracy,auto_arima_accuracy,
+                    simple_exp_accuracy,double_exp_accuracy,
+                    triple_exp_accuracy,tbat_accuracy,manual_accuracy) 
 
 # Turnover
 
