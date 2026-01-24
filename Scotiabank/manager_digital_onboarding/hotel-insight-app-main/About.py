a76ed800d6f91fbaf735422cb7d40ac3ff2bfdc1
Edima <edimaudo@gmail.com>
2026-01-24 03:02:27 -0500

    Create About.py

---
+++ Scotiabank/manager_digital_onboarding/hotel-insight-app-main/About.py
@@ -0,0 +1,11 @@
+from utils import *
+
+st.title(APP_NAME)
+st.header(ABOUT_HEADER)
+
+st.markdown(
+    """
+A hotel analytics tool focused on focused on outlining business KPIs and trends for hotel management.
+
+"""
+)
